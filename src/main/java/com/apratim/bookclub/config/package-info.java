2a6cacf102876f41f38b2aaef3497f0d7a9a351b
apratimnath7 <apratimnath7@gmail.com>
2022-04-06 01:07:55 +0530

    Initial Commit: Generated app via JHipste + Open JFX dependencies

---
+++ src/main/java/com/apratim/bookclub/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.apratim.bookclub.config;
---
+++ src/main/java/com/apratim/bookclub/javafx/GUIStarter.java
@@ -0,0 +1,35 @@
+package com.apratim.bookclub.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * @author Cyntexia
+ *
+ */
+public class GUIStarter extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button button = new Button("Say, Hello World");
+
+		button.setOnAction(eventHandler -> {
+			System.out.println("Hello World");
+		});
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("New JavaFX Application");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
